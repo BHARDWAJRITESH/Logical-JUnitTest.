2249b816a6f4ea0eceb0af5fa148a83d51a1d589
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-21 11:40:07 +0530

    Add:- Checking whether number is perfect number or not

---
+++ src/com/Bridgelabs/PerfectNumber.java
@@ -0,0 +1,29 @@
+package com.Bridgelabs;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		
+		 long number;
+		 long sum=0;
+		 
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("Enter the number: ");
+		 number=scan.nextLong();
+		 
+		 int i=1;
+		 while( i <= number/2) {
+			 
+			 if(number%i ==0) {
+				 sum = sum + i;}
+			 i++;
+		 }
+		 if(sum == number) {
+				 System.out.println(number+ " is aperfect number");
+			 }else
+				 System.out.println(number+ "is not a perfect number");
+		 }
+	
+}
